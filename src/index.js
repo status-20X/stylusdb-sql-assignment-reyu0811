682bf1291c1abefcf8bfec043e93d08a1617863e
reyu0811 <117917054+reyu0811@users.noreply.github.com>
2024-05-08 15:11:55 +0530

    "Hello8"

--- src/index.js
+++ src/index.js
@@ -4,8 +4,28 @@ const readCSV = require('./csvReader')
 
 async function executeSELECTQuery(query) {
     try {
-        const { fields, table, whereClauses } = parseQuery(query);
-        const data = await readCSV(`${table}.csv`);
+        const { fields, table, whereClauses, joinTable, joinCondition } = parseQuery(query);
+        let data = await readCSV(`${table}.csv`);
+
+        if (joinTable && joinCondition) {
+            const joinData = await readCSV(`${joinTable}.csv`);
+            data = data.flatMap(mainRow => {
+                return joinData
+                    .filter(joinRow => {
+                        const mainValue = mainRow[joinCondition.left.split('.')[1]];
+                        const joinValue = joinRow[joinCondition.right.split('.')[1]];
+                        return mainValue === joinValue;
+                    })
+                    .map(joinRow => {
+                        return fields.reduce((acc, field) => {
+                            const [tableName, fieldName] = field.split('.');
+                            acc[field] = tableName === table ? mainRow[fieldName] : joinRow[fieldName];
+                            return acc;
+                        }, {});
+                    });
+            });
+        }
+
 
         function evaluateCondition(row, clause) {
             const { field, operator, value } = clause;
